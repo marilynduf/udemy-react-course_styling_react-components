9e50c32929b13bbe7947787ddb728eefacfba3e4
M <marilynduf@gmail.com>
2025-08-30 21:50:17 -0400

    Creation du text button en styled-components

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -30,6 +30,28 @@ const Input = styled.input`
     border-radius: 0.25rem;
     box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
 `;
+const Button = styled.button`
+    padding: 1rem 2rem;
+    font-weight: 600;
+    text-transform: uppercase;
+    border-radius: 0.25rem;
+    color: #1f2937;
+    background-color: #f0b322;
+    border-radius: 6px;
+    border: none;
+    &:hover {
+        background-color: #f0920e;
+    }
+`;
+
+const TextButton = styled.button`
+    color: #f0b322;
+    border: none;
+
+    &:hover {
+        color: #f0920e;
+    }
+`;
 
 export default function AuthInputs() {
     const [enteredEmail, setEnteredEmail] = useState("");
@@ -76,12 +98,8 @@ export default function AuthInputs() {
                 </p>
             </ControlControler>
             <div className="actions">
-                <button type="button" className="text-button">
-                    Create a new account
-                </button>
-                <button className="button" onClick={handleLogin}>
-                    Sign In
-                </button>
+                <TextButton type="button">Create a new account</TextButton>
+                <Button onClick={handleLogin}>Sign In</Button>
             </div>
         </div>
     );
